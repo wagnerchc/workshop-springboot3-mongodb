2c3c5c2363826b8db2e69f6c3cb63d53c3eb079f
wagnerchc <w.chequeleiro@gmail.com>
2023-08-02 17:57:27 -0300

    AuthorDTO implementation

--- src/main/java/com/wpch/workshopmongo/config/Instantiation.java
+++ src/main/java/com/wpch/workshopmongo/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.wpch.workshopmongo.domain.Post;
 import com.wpch.workshopmongo.domain.User;
+import com.wpch.workshopmongo.dto.AuthorDTO;
 import com.wpch.workshopmongo.repository.PostRepository;
 import com.wpch.workshopmongo.repository.UserRepository;
 
@@ -34,9 +35,9 @@ public class Instantiation implements CommandLineRunner{
 		
 		postRepository.deleteAll();
 		
-		Post post1 = new Post(null, Instant.parse("2023-08-02T18:00:00Z"), "Mussum Ipsum", "Mussum Ipsum, cacilds vidis litro abertis.  Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Não sou faixa preta cumpadi, sou preto inteiris, inteiris. Suco de cevadiss deixa as pessoas mais interessantis. A ordem dos tratores não altera o pão duris.", maria);
-		Post post2 = new Post(null, Instant.parse("2023-08-03T18:00:00Z"), "Cacilds vidis litro abertis", "Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Cevadis im ampola pa arma uma pindureta. In elementis mé pra quem é amistosis quis leo. Detraxit consequat et quo num tendi nada.", maria);
-		Post post3 = new Post(null, Instant.parse("2023-08-04T18:00:00Z"), "Mussum Ipsum, cacilds vidis litro abertis", "Cevadis im ampola pa arma uma pindureta. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Si num tem leite então bota uma pinga aí cumpadi! Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus.", alex);
+		Post post1 = new Post(null, Instant.parse("2023-08-02T18:00:00Z"), "Mussum Ipsum", "Mussum Ipsum, cacilds vidis litro abertis.  Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Não sou faixa preta cumpadi, sou preto inteiris, inteiris. Suco de cevadiss deixa as pessoas mais interessantis. A ordem dos tratores não altera o pão duris.", new AuthorDTO(maria));
+		Post post2 = new Post(null, Instant.parse("2023-08-03T18:00:00Z"), "Cacilds vidis litro abertis", "Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Cevadis im ampola pa arma uma pindureta. In elementis mé pra quem é amistosis quis leo. Detraxit consequat et quo num tendi nada.", new AuthorDTO(maria));
+		Post post3 = new Post(null, Instant.parse("2023-08-04T18:00:00Z"), "Mussum Ipsum, cacilds vidis litro abertis", "Cevadis im ampola pa arma uma pindureta. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Si num tem leite então bota uma pinga aí cumpadi! Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus.", new AuthorDTO(alex));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2, post3));
 		
