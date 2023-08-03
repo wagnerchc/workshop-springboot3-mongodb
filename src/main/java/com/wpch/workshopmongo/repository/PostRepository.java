6d7e99970356ca0229aeab0d4cbf7a72e686c223
wagnerchc <w.chequeleiro@gmail.com>
2023-08-03 09:03:29 -0300

    Query methods for posts title

--- src/main/java/com/wpch/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/wpch/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.wpch.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.wpch.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
