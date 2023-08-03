7792bd7def66b9cfdae7a9740f686de8997de0c5
wagnerchc <w.chequeleiro@gmail.com>
2023-08-03 10:17:43 -0300

    Full search with query selector from MongoDB

--- src/main/java/com/wpch/workshopmongo/services/PostService.java
+++ src/main/java/com/wpch/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.wpch.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,5 +28,10 @@ public class PostService {
 	
 	public List<Post> findByTitleRegex(String text){
 		return repo.findByTitleRegex(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
